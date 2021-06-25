c4008e7d850a45144cb0c76dc2fae7b4e7b02733
Jacek Hacking <jacekhacking@Jaceks-MBP.localdomain>
2021-06-25 09:15:06 -0600

    added more questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project'
+    },
+    {
+        type: 'list',
+        name: '',
+        message: ''
+    },
+    {
+        type: '',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command should be run to run tests?'
+    },
+    {
+        type: 'input',
+        name: '',
+        message: ''
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const filename = 'READEME.md'
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
